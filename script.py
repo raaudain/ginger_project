2c8f52632708b40d379f1a0ffaa91e4d99381844
Ramon Audain <raudain123@gmail.com>
2021-08-21 00:05:49 -0700

    able to push to aws bucket

--- script.py
+++ script.py
@@ -1,12 +1,9 @@
-# https://developers.ringcentral.com/my-account.html#/applications
-# Find your credentials at the above url, set them as environment variables, or enter them below
-
-import json
+import json, boto3
 from cred import var as v
 from ringcentral import SDK
 
 
-# PATH PARAMETERS
+# RingCentral credentials
 accountId = v.accountId
 clientId = v.clientId
 clientSecret = v.clientSecret
@@ -20,19 +17,25 @@ platform = sdk.platform()
 
 # Be sure to reset the password in Sandbox. It's different from your dev account credentials.
 platform.login(username, extension, password)
+# Use when you want to test RingCentral api
+bearer_token = platform._auth_header()
 res = json.loads(platform.get("/restapi/v1.0/account/~/extension/~/message-store").text())
 
 message_uri = ""
 
-# Get first message from response
+# Get latest message
 for i in res["records"][0]["attachments"]:
-    print(i)
     message_uri = i["uri"]
 
-bearer_token = platform._auth_header()
 voicemail = platform.get(message_uri)
 
 
-print(bearer_token, message_uri, voicemail)
+# print(bearer_token, message_uri, voicemail)
 
 
+client = boto3.client("s3", aws_access_key_id=v.aws_access_key, aws_secret_access_key=v.aws_secret_key)
+
+aws_bucket = "gingerproject"
+aws_file_name = "ringcentral_voicemail"
+
+client.upload_file("./README.md", aws_bucket, aws_file_name)
