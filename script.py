b9cd0053a8c940002622d0acabc3a95587b15af6
Ramon Audain <raudain123@gmail.com>
2021-08-22 14:59:32 -0700

    Add check if messages exist

--- script.py
+++ script.py
@@ -1,47 +1,60 @@
-import json, boto3, requests, os
-from cred import var as v
+import json, requests, boto3, os
 from ringcentral import SDK
+from cred.var import client_id, client_secret, server_url, username, extension, password, aws_access_key, aws_secret_key, s3_bucket, s3_object_url
 
 
-# RingCentral credentials
-accountId = v.accountId
-clientId = v.clientId
-clientSecret = v.clientSecret
-serverURL = v.serverURL
-username = v.username
-extension = v.extension
-password = v.password
+# Comment line 3 and uncomment below if you want to input credentials directly into script
+# # RingCentral credentials
+# client_id = ""
+# client_secret = ""
+# server_url = ""
+# username = ""
+# extension = ""
+# password = ""
 
-sdk = SDK(clientId, clientSecret, serverURL)
+# #AWS Credentials
+# aws_access_key = ""
+# aws_secret_key = ""
+# s3_bucket = ""
+# s3_object_url = ""
+
+sdk = SDK(client_id, client_secret, server_url)
 platform = sdk.platform()
 
 # Be sure to reset the password in Sandbox. It's different from your dev account credentials.
 platform.login(username, extension, password)
-# Use to inspect RingCentral api in Insomnia or Postman
+# Use token to inspect json with Insomnia or Postman
 bearer_token = platform._auth_header()
 res = json.loads(platform.get("/restapi/v1.0/account/~/extension/~/message-store").text())
 
-message_uri = ""
-file_name = ""
+# Checks if there are messages
+if len(res["records"]):
+    message_uri = ""
+    file_name = ""
+    # Get latest message
+    for i in res["records"][0]["attachments"]:
+        message_uri = i["uri"]
+        file_name = f"ringcentral_voicemail_{i['id']}.mp3"
 
-# Get latest message
-for i in res["records"][0]["attachments"]:
-    message_uri = i["uri"]
-    file_name = f"ringcentral_voicemail_{i['id']}.mp3"
+    # Get audio data
+    voicemail = requests.get(message_uri, headers={"Authorization":bearer_token}).content
 
-# Get audio data
-voicemail = requests.get(message_uri, headers={"Authorization":bearer_token}).content
+    mp3_file = "./" + file_name
 
-# Write audio data to .mp3 file
-with open("voicemail.mp3", "wb") as f:
-    f.write(voicemail)
+    # Write audio data to .mp3 file
+    with open(mp3_file, "wb") as f:
+        f.write(voicemail)
 
-client = boto3.client("s3", aws_access_key_id=v.aws_access_key, aws_secret_access_key=v.aws_secret_key)
+    client = boto3.client("s3", aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key)
 
-aws_bucket = "gingerproject"
+    # Upload file to AWS S3 bucket and allow public read access
+    client.upload_file(mp3_file, s3_bucket, file_name, ExtraArgs={"ACL":"public-read"})
 
-# Upload file to AWS S3 bucket and allow public read access
-client.upload_file("./voicemail.mp3", aws_bucket, file_name, ExtraArgs={"ACL":"public-read"})
+    # Print Object URL
+    print("Object URL: " + s3_object_url + file_name)
 
-# Delete .mp3 file
-os.remove("./voicemail.mp3")
+    # Delete .mp3 file
+    os.remove(mp3_file)
+else:
+    print("No messages")
+
